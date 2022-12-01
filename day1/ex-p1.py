170f12eb9be4b8b2dcd27e1c242dd0ae15f3e403
Syan Delbart <syan.delbart@hotmail.com>
2022-12-01 13:08:52 +0100

    day1

---
+++ day1/ex-p1.py
@@ -0,0 +1,13 @@
+
+max_calories = 0
+current_calories = 0
+
+for line in open("./day1/input","r"):
+  if line is "\n":
+    if(current_calories > max_calories):
+      max_calories = current_calories
+    current_calories = 0
+    continue
+  current_calories += int(line)
+
+print(max_calories)
---
+++ day1/ex-p2.py
@@ -0,0 +1,14 @@
+
+current_calories = 0
+
+calories = []
+
+for line in open("./day1/input","r"):
+  if line == "\n":
+    calories.append(current_calories)
+    current_calories = 0
+    continue
+  current_calories += int(line)
+
+calories.sort(reverse=True)
+print(sum(calories[0:3]))
