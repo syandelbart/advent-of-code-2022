d0668f2ced69d0fc37cac1e641359ca44a792e2a
Syan Delbart <syan.delbart@hotmail.com>
2022-12-06 11:21:52 +0100

    day6 solution added

---
+++ day6/ex.py
@@ -0,0 +1,13 @@
+# EX2 -> marker_length = 14
+marker_length = 4
+letter_buffer = set()
+
+for line in open("./day6/input","r"):
+    line_formatted = line.strip()
+    line_chunks = list(line)
+    for index in range(marker_length,len(line_chunks) - marker_length) :
+        if len(set(line_chunks[index: index+marker_length])) == marker_length:
+            # print index AFTER last character, not first
+            print(index + marker_length)
+            break
+        
