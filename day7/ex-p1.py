cab540bcd063eee89f1cd9868b368e783b708824
Syan Delbart <syan.delbart@hotmail.com>
2022-12-07 14:04:26 +0100

    part a day 7 lol finally...

---
+++ day7/ex-p1.py
@@ -0,0 +1,85 @@
+import re
+from enum import Enum
+
+class Type(Enum):
+    DIR = 1
+    FILE = 2
+
+class File:
+    def __init__(self, name : str, size : int, parent,type : Type = Type.DIR):
+        self.name = name
+        self.size = size
+        self.parent = parent
+        self.type = type
+        if(self.parent):
+            self.parent.add_children(self)
+        self.children = list()
+    def get_name(self):
+        return self.name
+    def get_parent(self):
+        return self.parent
+    def get_children(self):
+        return self.children
+    def add_children(self, child):
+        self.children.append(child)
+    def get_size(self):
+        return self.size
+    def is_file(self):
+        return self.type == Type.FILE
+
+max_file_size = 100000
+size_total = 0
+
+def print_structure(file,depth = 0):
+    global size_total
+    print("|","F" if file.is_file() else "D" ,"-" * depth,file.name, file.get_size(),)
+
+    size = file.get_size()
+
+    for child in file.get_children():
+        size += print_structure(child,depth + 1)
+    
+    if not file.is_file():
+        if size <= 100000:
+            size_total += size
+
+    return size
+
+cur = File("/",0,None)
+root = cur
+
+for line in open("./day7/input-2","r"):
+    line_formatted = line.strip()
+    line_chunks = list(line)
+
+    match = re.findall(r'\w+\.\w+|\w+|/|\.\.',line_formatted)
+
+    arg1 = match[0]
+
+    if arg1 == "ls":
+        # print("ls",match)
+        continue
+    elif arg1 == "cd":
+        # print("cd",match)
+        arg2 = match[1]
+        if arg2 == "..":
+            cur = cur.get_parent()
+        elif arg2 == "/":
+            cur = root
+        else:
+            for child in cur.get_children():
+                if child.name == arg2:
+                    cur = child
+
+
+    elif arg1 == "dir":
+        # print("dir",match)
+        dir = File(match[1],0,cur,type=Type.DIR)
+
+    else:
+        file = File(match[1],int(arg1),cur,type=Type.FILE)
+
+print(print_structure(root))
+print("Size:",size_total)
+
+
